3451478ab758e0455a13a0d345a2a33e75550394
ipincamp <support@nur-arifin.my.id>
2024-06-15 19:45:45 +0700

    feat: add FunctionalComponent

---
+++ src/components/FunctionalComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FunctionalComponent = () => {
+  return (
+    <div>
+      <h2>Ini adalah Functional Component</h2>
+      <p>Functional Component itu simple dan mudah digunakan.</p>
+    </div>
+  );
+};
+
+export default FunctionalComponent;
