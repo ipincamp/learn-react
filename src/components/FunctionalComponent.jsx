964a9093c03e58c44986939decbd35fde41d583c
ipincamp <support@nur-arifin.my.id>
2024-06-15 21:27:02 +0700

    feat: add input and buttons for incrementing and decrementing count in FunctionalComponent

--- src/components/FunctionalComponent.jsx
+++ src/components/FunctionalComponent.jsx
@@ -4,13 +4,34 @@ const FunctionalComponent = (props) => {
   const numValue = parseInt(props.numValue);
   const [count, setCount] = useState(numValue);
 
+  const [inputValue, setInputValue] = useState(0);
+
+  const handleInputChange = (event) => {
+    setInputValue(Number(event.target.value));
+  };
+
+  const increment = () => {
+    setCount(count + inputValue);
+  };
+
+  const decrement = () => {
+    setCount(count - inputValue);
+  };
+
   return (
     <div>
       <h2>Ini adalah Functional Component</h2>
       <p>{props.message}</p>
 
       <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={increment}>Tambah [+]</button>
+      <input
+        type="number"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Masukkan angka"
+      />
+      <button onClick={decrement}>Kurang [-]</button>
     </div>
   );
 };
