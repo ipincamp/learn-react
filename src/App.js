0cacb2beb034c9666c6abbe81b48475fa3f7c286
ipincamp <support@nur-arifin.my.id>
2024-06-15 20:33:27 +0700

    feat: add param to message prop for MainContent component
    Argumen yang diberikan berasal dari App Component, kemudian diteruskan ke MainContent Component.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <MainContent />
+      <MainContent message="Hello World dari App Component!" />
       <Footer />
     </div>
   );
