8e41cffad31e10bb35b918ad4a8facfb07d239b1
ipincamp <support@nur-arifin.my.id>
2024-06-15 19:55:20 +0700

    feat: add Header component
    Menggunakan gaya penulisan Functional Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div>Welcome to the React Practice Module</div>
+    </header>
+  );
+};
+
+export default Header;
