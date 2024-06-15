77098525f1b603c960c9cb98a9193e0bc810f0c7
ipincamp <support@nur-arifin.my.id>
2024-06-15 21:10:32 +0700

    feat: add count state and increment button to FunctionalComponent
    Menggunakan state untuk interaksi input dari user, variable count adalah nilai sekarang, dan fungsi setCount digunakan untuk merubah nilai dari variable count. Nilai defaultnya adalah 0 dengan menginisiasi useState(0).

--- src/components/FunctionalComponent.jsx
+++ src/components/FunctionalComponent.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const FunctionalComponent = (props) => {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
       <h2>Ini adalah Functional Component</h2>
       <p>{props.message}</p>
+
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
   );
 };
